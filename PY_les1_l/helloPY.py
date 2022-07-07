38a389db35a2bb7b8d65938a494fbc8ff380e82a
Ramka <whereisourgod@gmail.com>
2022-07-07 18:39:24 +0300

    l1

---
+++ PY_les1_l/helloPY.py
@@ -0,0 +1,27 @@
+from re import A
+
+
+#print('Hello?')
+#a = 123
+#b = a * a
+#print(a + b)
+#print(a,b)
+#print('{1} - {0}'.format(a,b))
+#print(f'{a} - {b}')
+
+# f = false
+#list = [1,2,3,4,5,6]        #Massive
+#print(list) 
+#print("any")
+#a = input()
+#print(a)
+#print("any")
+#a = int(input())    #Явная типизация
+#print(a)
+#a = +123     # унарный плюс
+#b = -124     # унарный минус
+#c = a + b
+#a = 1.2
+#b = 12
+#c = round(a * b, 2) #2 знака после запятой
+
