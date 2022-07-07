2a2b86c80ade1534fee8d2e06953dd31bac5681c
Ramka <whereisourgod@gmail.com>
2022-07-07 20:11:50 +0300

    task3

---
+++ PY_les1_s/task3.py
@@ -0,0 +1,13 @@
+# input N. Print -N to N
+
+num = int(input("Which N? "))
+for i in range(-num, num + 1):
+    print(i)
+
+# кратно ли число 5 и 19 или 15, но не 30
+if((num % 5 == 0) and (num % 10 == 0)):
+    print(f"{num} кратно 5 и 10")
+elif((num % 15 ==0) and (num % 30 != 0)):
+    print(f"{num} кратно 15 но не 30")
+else:
+    print("zzzzz")
