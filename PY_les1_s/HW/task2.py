fce376dbf585344a7696cd0eb26e06d38a5eef8a
Ramka <whereisourgod@gmail.com>
2022-07-08 08:57:42 +0300

    task2_f

--- PY_les1_s/HW/task2.py
+++ PY_les1_s/HW/task2.py
@@ -1,26 +1,9 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 def function(any1, any2, any3):
-    if(any1 == 0):
-        if((-(any2 or any3)) == -any2 and -any3):
-            print("True")
-        else:
-            print("False")
-    elif(any2 == 0):
-        if((-(any1 or any3)) == -any1 and -any3):
-            print("True")
-        else:
-            print("False")
-    elif(any3 == 0):
-        if((-(any2 or any1)) == -any2 and -any1):
-            print("True")
-        else:
-            print("False")
-
+    if(not (any1 or any2 or any3) == (not any1 and not any2 and not any3)):
+        print("True")
     else:
-        if((-(any1 or any2 or any3)) == -any1 and -any2 and -any3):
-            print("True")
-        else:
-            print("False")
+        print("False")
 
 
 
