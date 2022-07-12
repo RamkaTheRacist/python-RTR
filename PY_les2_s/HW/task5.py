a43bb7249e950f20acc6e0bdb80ae573291468af
Ramka <whereisourgod@gmail.com>
2022-07-12 18:37:46 +0300

    task5

---
+++ PY_les2_s/HW/task5.py
@@ -0,0 +1,28 @@
+#Реализуйте алгоритм перемешивания списка.
+import random
+
+def fillArrayRandom(size: int, startNumber: int, endNumber: int):
+    array = []
+    for i in range(size + 1):
+        array.append(random. randrange(startNumber, endNumber))
+    return array
+
+
+def shuffle(array: int):
+    tmp = 0
+    tmpIndex = 0
+    for i in range(len(array)):
+        tmp = array[i]
+        tmpIndex = random. randrange(len(array))
+        array[i] = array[tmpIndex]
+        array[tmpIndex] = tmp
+    return array
+
+
+size = int(input("Which size of array? "))
+startNumber = int(input("Fill array from which number? "))
+endNumber = int(input("Fill array to which number? "))
+array = fillArrayRandom(size, startNumber, endNumber)
+print(f"Was: {array}")
+array = shuffle(array)
+print(f"Is:  {array}")
