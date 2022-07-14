00b4a5a5408b9eac3d34c55d5c10e37f3a9038d0
Ramka <whereisourgod@gmail.com>
2022-07-14 08:53:19 +0300

    task1

---
+++ PY_les2_l/task1.py
@@ -0,0 +1,20 @@
+
+
+
+# with open('file.txt', 'a') as data:
+#     data.write("\nAny\n")
+#     data.write("\nCheck\n")
+
+# text = ['addd', 'arfesf', 'fasdas']
+# data = open('file.txt', 'a')   #file name and using type
+# #data.writelines(text)
+# data.write("\nNew Line\n")
+# data.write("\nAnother\n")
+# data.close()
+
+
+path = 'file.txt'
+data = open(path, 'r')
+for line in data:
+    print(line)
+data.close()
