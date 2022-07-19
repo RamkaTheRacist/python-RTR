eb40136917fc6b2b042454aa0d4ca6a86381b49f
Ramka <whereisourgod@gmail.com>
2022-07-19 14:50:07 +0300

    rT

--- PY_les4_s/HW/task2.py
+++ PY_les4_s/HW/task2.py
@@ -1,22 +1,4 @@
 #Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
-def removeRepetiton(array:list, array2:list) -> list:
-    for i in range(len(array2)):
-        del array[array[len(array2) - i]]
-    return array
-
-
-def positionOfRepetition(array: list) -> list:
-    posArray = []
-    k = 0
-    for i in range(1, len(array)):
-        if(array[k] == array[i]):
-            posArray.append(i)
-        else:
-            k+=1
-    return posArray
-
-
-
 def division(number: int) -> list:
     tmp = number
     array = [1]
@@ -33,6 +15,4 @@ def division(number: int) -> list:
 
 num = int(input("Which number? "))
 print(f"Result: {division(num)}")
-# arrayDirty = division(num)
-# arrayPosition = positionOfRepetition(arrayDirty)
-# print("Result: {removeRepetiton(arrayDirty, arrayPosition)}")
+
