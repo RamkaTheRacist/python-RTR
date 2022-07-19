c669f8b767f903d9a6b2c10b214679f1bdabb522
Ramka <whereisourgod@gmail.com>
2022-07-19 14:50:34 +0300

    rT

--- PY_les4_s/HW/task3.py
+++ PY_les4_s/HW/task3.py
@@ -1,37 +1,5 @@
 #Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 import random
-def removeRepetiton(array:list, array2:list) -> list:
-    for i in range(len(array2)):
-        del array[array2[len(array2) - 1 - i]]
-    return array
-
-
-def positionOfRepetition(array: list) -> list:
-    posArray = []   
-    for i in range(len(array)):
-        for k in range(i + 1, len(array)):
-            if(array[k] == array[i]):
-                posArray.append(k)
-                break    
-    return posArray
-
-
-def sortOfArray(array: list) -> list:
-    index = 0
-    while(index < len(array)):
-        max = -1
-        tmpPos = len(array) - 1 - index
-        for i in range(len(array) - index):
-            if(max < array[i]):
-                max = array[i]
-                tmpPos = i
-        if(tmpPos != len(array) - index):
-            tmp = array[len(array) - 1 - index]
-            array[len(array) - 1 - index] = array[tmpPos]
-            array[tmpPos] = tmp
-        index += 1
-    return array
-
 
 def fillArrayRandom(size: int, startNumber: int, endNumber: int) -> list:
     array = []
@@ -40,8 +8,20 @@ def fillArrayRandom(size: int, startNumber: int, endNumber: int) -> list:
     return array
 
 array = fillArrayRandom(int(input("Which size? ")), int(input("From? ")), int(input("To? ")))
-print(array)
-posArray = positionOfRepetition(array)
-sortedArray = sortOfArray(posArray)
-print("Now:")
-print(removeRepetiton(array, sortedArray))
+print(f"Was: {array}")
+bool = True
+k = 0
+while(bool == True):
+    count = array.count(array[k])
+    if(count > 1):
+        check = array[k]
+        for i in range(count):
+            array.remove(check)
+    elif(count == 1):
+        k+=1
+    if(k > (len(array) - 1)):
+        bool = False
+if(len(array) != 0):
+    print(f"Is: {array}")
+else:
+    print("Nothing here")
