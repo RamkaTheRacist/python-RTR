6a0a6682eddb95050b02658c6f6af5656c511e8f
Ramka <whereisourgod@gmail.com>
2022-07-29 09:22:42 +0300

    task4

---
+++ PY_les6_s/HW/task4.py
@@ -0,0 +1,31 @@
+#Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+import random
+
+def fillArrayRandom(size: int, startNumber: int, endNumber: int) -> list:
+    array = []
+    for i in range(size):
+        array.append(random. randrange(startNumber, (endNumber + 1)))
+    return array
+
+# array = fillArrayRandom(int(input("Which size? ")), int(input("From? ")), int(input("To? ")))
+# print(f"Was: {array}")
+# bool = True
+# k = 0
+# while(bool == True):
+#     count = array.count(array[k])
+#     if(count > 1):
+#         check = array[k]
+#         for i in range(count):
+#             array.remove(check)
+#     elif(count == 1):
+#         k+=1
+#     if(k > (len(array) - 1)):
+#         bool = False
+# if(len(array) != 0):
+#     print(f"Is: {array}")
+# else:
+#     print("Nothing here")
+array = fillArrayRandom(int(input("Which size? ")), int(input("From? ")), int(input("To? ")))
+print(f"Was: {array}")
+result = [array[i] for i in range(len(array)) if array.count(array[i]) == 1]
+print(f"Is: {result}")
